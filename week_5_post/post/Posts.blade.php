02cf225717ee0b22a053d35404ed89b483c3bdbc
jelenamihajlovic24 <54357038+jelenamihajlovic24@users.noreply.github.com>
2019-10-25 00:05:23 +0200

    Add files via upload

--- week_5_post/post/Posts.blade.php
+++ week_5_post/post/Posts.blade.php
@@ -1,47 +1,39 @@
 <h1>Users Posts</h1>
 <table style="width:70%; border:1px solid black;">
     <tr>
-        
         <th>Title</th>
         <th>Content</th>
         <th>Like</th>
         <th>Edit</th>
         <th>Delete</th>
     </tr>
-    
     @foreach($posts as $post)
         <tr>
-        
-            
             <td>{{$post->title}}</td>
             <td>{{$post->content}}</td>
-            <td>{{$post->likes}}</td>
-           
+            <td>{{$post->likes}}</td> 
         </tr>
-        
         <tr>
             <td>
-                <a href="update/{id}{{$post->id}}/edit">
+                <a href="posts/edit/{{$post->id}}">
                     <button type="button" autofocus> Edit/update user </button>
                 </a>
             </td>
             <td>
-                <a href="like/{{$posts->id}}/like">
+                <a href="posts/like/{{$post->id}}">
                     <button type="submit" autofocus> Like </button>
                 </a> 
             </td>
             <td>
-                <form method="POST" action="delete/{id}{{$posts->id}}">
-                    @method('DESTROY')
-                    @csrf
-                    <button type="button" autofocus> Delete</button>
-                </form>
+                <a href="posts/delete/{{$post->id}}">
+                    <button type="submit" autofocus> Delete </button>
+                </a> 
             </td>
         </tr>
-        @endforeach  
+    @endforeach  
 </table>
 <br/>
 
-<a href="/create">
+<a href="create">
     <button type="button" autofocus> Add a new Comment </button>
 </a>
